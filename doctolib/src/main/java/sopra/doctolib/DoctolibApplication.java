ff38d06d9456786dc0f741b668bfc647566d7bf9
manoncharles <charles.manon@yahoo.com>
2020-10-27 11:48:35 +0100

    spring boot

---
+++ doctolib/src/main/java/sopra/doctolib/DoctolibApplication.java
@@ -0,0 +1,13 @@
+package sopra.doctolib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctolibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctolibApplication.class, args);
+	}
+
+}
---
+++ doctolib/src/test/java/sopra/doctolib/DoctolibApplicationTests.java
@@ -0,0 +1,13 @@
+package sopra.doctolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctolibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
